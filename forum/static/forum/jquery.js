831155c464a9e6491e42bdfb6fd053977bd4166b
asingjr2 <asingjr2@gmail.com>
2018-02-21 23:50:49 -0500

    failed downvote still, added what i could

--- forum/static/forum/jquery.js
+++ forum/static/forum/jquery.js
@@ -1,4 +1,26 @@
-$(document).ready(function () {
-    alert('starting reddit clone');
-    // Add in more as required
-})
+// NEED TO CHANGE NAME 
+
+// $(document).ready(function() {
+//     $("#jq_test").click(function(){
+//         alert("jquery works");
+//     })
+// });
+
+//AJAX to ADD
+// $("#form").submit(function (event) {
+//     event.preventDefault();
+//     var data = $("#form").serialize();
+
+//     $.ajax({
+//         method: "POST",
+//         url: "create",
+//         data: data,
+//         success: function (res) {
+//             console.log("hey");
+//         }
+//     })
+// })
+
+
+
+
--- forum/views.py
+++ forum/views.py
@@ -7,63 +7,55 @@ from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from .forms import RegisterForm, LoginForm
 
 
-# Can change "standard query reference from object_list to whatever as below"
-class HomePageView(ListView):
-    template_name = "forum/list.html"
-    context_object_name= "post_list"
+# Serving as entry point until user login status is implemented
+class AnonPageView(ListView):
+    queryset = Forum.objects.all()
+    # Change back to standard "forum_list"
+    template_name = "forum/user_subreddits.html"
+    context_object_name= "forum_list"
 
-    # Throwing error on my end for comment, but view works
-    #Should be first page
-    def get_queryset(self):
-        # Not sure how to pass below...need to refractor into class createview maybe?  Sending context forward as is creates error
-        vote_count = Post.objects.first().votes.count()
-        upvotes = Post.objects.first().votes.filter(vote=1).count()
-        downvotes = Post.objects.first().votes.filter(vote=-1).count()
-        total = upvotes - downvotes
-        context = {
-            "vc": vote_count,
-            "total":total
-        }
-        print(context)
-        return Post.objects.all()
+# User page with no subreddit set to home (list of forums)
+class UserSubredditsView(ListView):
+    queryset = Forum.objects.all()
+    template_name = "forum/user_subreddits.html"
+    context_object_name = "forum_list"
 
+# Could default to DBZ forum or whatever once set
+class ForumView(DetailView):
+    model = Forum
 
-def posttest(request):
-    if request.method == "POST":
-        try:
-            user = request.user
-            post = Post.objects.get(id=request.POST["post_id"])
-            vote = request.POST["vote_choice"]
-            PostVote.objects.create(
-                user= user,
-                post= post,
-                vote= vote
-                )
-        except:
-            print("could not create")
-        return redirect("/")
+class PostView(DetailView):
+    model = Post
 
-# Detail view containing some specific user data...could evolve to user preferences page
-class UserView(DetailView):
-    model = User
-
-    def get(self, request):
-        user = request.user
-        context = {
-            "user":user, 
-            "test": "test"
-        }
-        return render(request, "forum/user.html", context)
-
-# Try to use with url para, but page throws error regarding UUID
-class CommentDetailView(DetailView):
+class CommentView(DetailView):
     model = Comment
-    template_name= "forum/comment.html"
 
-# Can ignore as we can use package as recommended
+# Tried old way and print to see if info as being sent
+def create_postvote(request, pk, slug ):
+    if request.method == 'POST':
+        print(pk)
+        print(slug)
+        print(request.Post["user"])
+        print(request.Post["id"])
+        print(request.Post["slug"])
+        print(request.Post["downvote"])
+        return redirect("user_sub")
+'''
+**********STUCK ON CREATION PART*******
+have not been able to find way to create with using model forms...and 
+class CPracView(View):
+    def post(self, request):
+        if request.method == 'POST':
+            print(request.Post["id"])
+            print(request.Post["slug"])
+            return redirect("user_sub")
+            
+    def get(self, request):
+        forum = Forum.objects.first()
+        context = {
+            "forum":forum
+        }
+        return render(request,"forum/forum_detail.html", context )
+ 
+'''
 
-
-# Not sure how to pass information through that I can access (i.e object_name= my_name) and then just use string injection/interpolation in html
-# class MainView(generic.DetailView):
-#     template_name = "forum/main.html"
-    
